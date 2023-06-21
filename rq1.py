43288b76846ad0afb450321efd716a7432af49d1
Tai D. Nguyen <duytai.cse@gmail.com>
2023-06-21 18:46:23 +0800

    Add HT

--- rq1.py
+++ rq1.py
@@ -3,5 +3,8 @@ from a18z import inject, rq1
 # inject('RQ1/BAT.sol')
 # rq1('RQ1/BAT.sol.sol')
 
-inject('RQ1/BNB.sol')
-rq1('RQ1/BNB.sol.sol')
+# inject('RQ1/BNB.sol')
+# rq1('RQ1/BNB.sol.sol')
+
+inject('RQ1/HT.sol')
+rq1('RQ1/HT.sol.sol')
